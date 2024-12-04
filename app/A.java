1de9ec2601f2e53e2a67c9fbd940c51ed313d0a7
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-03 22:56:20 -0300

    03DEZ24 - #App Netflix - Disposição inicial e adicionando Drawables.

---
+++ app/A.java
@@ -0,0 +1,5 @@
+Button botao = findViewById(R.id.botao_salvar);
+        botao.setOnClickListener(v -> System.out.println("Botão foi clicado!"));
+
+Button botao = findViewById(R.id.botao_salvar);
+        botao.setOnClickListener(v -> System.out.println("Botão foi clicado!"));
